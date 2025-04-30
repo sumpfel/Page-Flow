10ccd12f400d8fcf70d6ad1252262df53c3741aa
sumpfel <christof.schaffer@schaffer-se.at>
2025-04-30 16:07:39 +0200

    first commit

---
+++ Page-Flow/Page-Flow/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Page_Flow
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
